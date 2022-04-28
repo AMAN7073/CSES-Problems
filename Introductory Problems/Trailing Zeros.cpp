f5fe6a6adb77e8dcd7db9deab53becaec07ce82d
AMAN7073 <74101312+AMAN7073@users.noreply.github.com>
2022-04-28 18:45:09 +0530

    Create Trailing Zeros.cpp

---
+++ Introductory Problems/Trailing Zeros.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main()
+{
+    long int n;cin>>n;
+    int count = 0;
+    for(int i=5;n/i>=1;i *=5){
+        count = count + n/i;
+    }
+ 
+    cout<<count;
+    
+}
